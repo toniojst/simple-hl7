0e24cf6f01ebaac530b213ba69acde5410d75b81
toniojst <toni.ojstersek93@gmail.com>
2022-02-08 23:49:33 +0100

    Update adt.js

--- examples/adt.js
+++ examples/adt.js
@@ -1,40 +1,24 @@
 var hl7 = require('../lib');
 
-////////////////////CLIENT///////////////////
-var parser = new hl7.Parser({segmentSeperator: '\n'});
-var client = hl7.Server.createTcpClient({
-  host: 'localhost',
-  port: 7777,
-  keepalive: true,
-  callback: function(err, ack) {
-    if (err) {
-      console.log("*******ERROR********");
-      console.log(err.message);
-    } else {
-      console.log(ack.log());
-    }
-  }
+///////////////////CLIENT/////////////////////
+var client = hl7.Server.createTcpClient('193.95.245.71', 3402);
+
+//create a message
+var msg = new hl7.Message(
+                    "EPIC",
+                    "EPICADT",
+                    "SMS",
+                    "199912271408",
+                    "CHARRIS",
+                    ["ADT", "A04"], //This field has 2 components
+                    "1817457",
+                    "D",
+                    "2.5"
+                );
+
+console.log('******sending message*****')
+client.send(msg, function(err, ack) {
+  console.log('******ack received*****')
+  console.log(ack.log());
 });
-
-var msg = parser.parseFileSync('test/samples/adt.hl7');
-
-console.log('************sending 1 message****************');
-client.send(msg);
-
-setTimeout(function() {
-  console.log('2');
-  console.log('************sending 2 message****************');
-  client.send(msg);
-}, 1000);
-
-setTimeout(function() {
-  console.log('************sending 3 message****************');
-  client.send(msg);
-}, 2000);
-
-
-setTimeout(function() {
-  process.exit();
-}, 5000)
-
-////////////////////CLIENT///////////////////
+///////////////////CLIENT/////////////////////
